470f0fe7950e72e3403e701c33cb91327c69d173
NatSjm <nat.simon319@gmail.com>
2026-07-05 20:42:24 +0200

    project base

---
+++ firefly-be/src/main/kotlin/com/firefly/fireflybe/FireflyBeApplication.kt
@@ -0,0 +1,11 @@
+package com.firefly.fireflybe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FireflyBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<FireflyBeApplication>(*args)
+}
---
+++ firefly-be/src/test/kotlin/com/firefly/fireflybe/FireflyBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.firefly.fireflybe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FireflyBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
